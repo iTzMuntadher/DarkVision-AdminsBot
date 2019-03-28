e6979c0c0136accf26f69d61094902582cba8cb9
iTzMuntadher <41391602+iTzMuntadher@users.noreply.github.com>
2019-03-28 10:47:36 +0300

    Update hi.js

--- hi.js
+++ hi.js
@@ -1,8 +1,12 @@
+const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = '$'
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
 
 client.on('message', message => {
-    if (message.content === 'hi') {
-    	message.reply('Hello');
+    if (message.content === 'hello') {
+    	message.reply('hi');
   	}
 });
