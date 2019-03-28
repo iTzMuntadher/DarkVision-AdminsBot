2bae6a6cf5df09a26a9c545eab7b90c8a0f9fdf9
iTzMuntadher <41391602+iTzMuntadher@users.noreply.github.com>
2019-03-28 11:06:03 +0300

    Create hi.js

---
+++ hi.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+var prefix = '$'
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    if (message.content === prefix + 'hi') {
+    	message.reply('hello');
+  	}
+});
+
+// THIS  MUST  BE  THIS  WAY
+client.login('NTYwNjk0MzczNTgzNjgzNjA0.D34DBg.8U3gRTGzVawh7Qs1Px70hWc3i9M');
