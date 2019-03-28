615abc2688439a4f52cb24c746cce8aab63a5dba
iTzMuntadher <41391602+iTzMuntadher@users.noreply.github.com>
2019-03-28 11:40:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,11 +7,11 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-  // If the message is "what is my avatar"
-  if (message.content.startsWith(prefix + 'avatar')) {
-    // Send the user's avatar URL
-    message.channel.sendmessage(message.author.avatarURL);
-  }
+	if (message.author === client.user) return;
+	if (message.content.startsWith(prefix + 'ping')) {
+		message.channel.sendMessage('pong');
+	}
+});
 
 // Log our bot in using the token from https://discordapp.com/developers/applications/me
 client.login('NTYwNjk0MzczNTgzNjgzNjA0.D34VcQ.2VhCbRLHRLgvoDaHX3Yzf8y_ZUw');
