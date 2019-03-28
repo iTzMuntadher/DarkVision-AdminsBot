03b3fff093ce41ab295b98c75e73471b0c160431
iTzMuntadher <41391602+iTzMuntadher@users.noreply.github.com>
2019-03-28 11:01:34 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+var prefix = '$'
 
 client.on('ready', () => {
     console.log('I am ready!');
@@ -10,6 +11,12 @@ client.on('message', message => {
     	message.reply('pong');
   	}
 });
+
+client.on('message', message => {
+    if (message.content === 'hi'){
+        message.reply('hello');
+    }
+}
 
 // THIS  MUST  BE  THIS  WAY
 client.login('NTYwNjk0MzczNTgzNjgzNjA0.D34DBg.8U3gRTGzVawh7Qs1Px70hWc3i9M');
